52d49f163dcde9dcefcf05c2328d2986779f43df
dmxruser <lenmndza@gmail.com>
2025-11-02 16:45:44 +1300

    fix???

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import '@patternfly/patternfly/patternfly.css';
 import './index.css';
+import '@patternfly/react-core/dist/styles/base.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-);
+)
